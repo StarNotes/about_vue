20efc7a333324965bcf18a9321c48471e3afff14
soliloquy <354882699@qq.com>
2018-10-05 16:49:49 +0800

    添加icon&iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,16 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
     export default {
-
+        props:['icon','iconPosition']
     }
 </script>
 <style lang="scss">
@@ -14,6 +21,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background-color: var(--button-bg);
+        vertical-align: middle;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         &:hover{
             border-color: var(--border-color-hover);
         }
@@ -22,6 +33,24 @@
         }
         &:focus{
             outline: none;
+        }
+        > .icon{
+            order: 1;
+            margin-left: 0;
+            margin-right: .3em;
+        }
+        > .content{
+            order: 2;
+        }
+        &.icon-right{
+            > .icon{
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+            > .content{
+                order: 1;
+            }
         }
     }
 </style>
