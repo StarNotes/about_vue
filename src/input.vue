f6d66aef70b5950b69b26275c8097501d5d639cb
soliloquy <354882699@qq.com>
2018-10-21 21:20:17 +0800

    双向绑定

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="wrapper" :class="{error}">
-        <input :value="value" :disabled="disable" :readonly="readonly" type="text"/>
+        <!--值(value)+事件(change) ==> v-model-->
+        <input :value="value" :disabled="disable" :readonly="readonly" type="text"
+               @change="$emit('change', $event.target.value)"
+               @input="$emit('input', $event.target.value)"
+               @focus="$emit('focus', $event.target.value)"
+               @blur="$emit('blur', $event.target.value)"
+        />
         <template v-if="error">
             <icon name="error" class="icon-error"></icon>
             <span class="errorMessage">{{error}}</span>
