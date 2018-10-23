88a703eddadcce9ea3925123cc8be6b0adf88b65
soliloquy <354882699@qq.com>
2018-10-23 14:19:15 +0800

    row 和 col基本实现

---
+++ src/col.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]" :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
+        <div style="border: 1px solid blue;height: 100px">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name:"GCol",
+        props:{
+            span:{
+                type:[Number,String]
+            },
+            offset:{
+                type:[Number,String]
+            }
+        },
+        data(){
+            return{
+                gutter:{
+                    type:[Number,String]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .col{
+        height: 100%;
+
+        width: 50%;
+
+        $class-prefix:col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+        $class-prefix:offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row" :style="{marginLeft:- gutter/2+'px',marginRight:- gutter/2+'px'}">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:"GRow",
+        props:{
+            gutter:{
+                type:[Number,String]
+            }
+        },
+        //内存中生成对象
+        created(){
+
+        },
+        //将对象挂到页面上
+        mounted(){
+            //子组件已append进来，且所有对象已挂载到页面上，可通过this.$children获取子元素
+            this.$children.forEach((vm)=>{
+                //通过父组件将gutter传到子组件
+                vm.gutter = this.gutter;
+            })
+
+        }
+    }
+</script>
+<style scoped lang="scss">
+   .row{
+       display: flex;
+   }
+</style>
